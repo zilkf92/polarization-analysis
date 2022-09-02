9dc974313d7191a90dfdc3301e5c67d1cf0f055f
Felix Zilk <zilk.felix@gmail.com>
2022-09-02 18:34:27 +0200

    fixed fitting, add return of key for min value

--- calibration.py
+++ calibration.py
@@ -9,32 +9,54 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def getKeysByValue(dictOfElements, valueToFind):
+    listOfKeys = list()
+    listOfItems = dictOfElements.items()
+    for item in listOfItems:
+        if item[1] == valueToFind:
+            listOfKeys.append(item[0])
+    return listOfKeys
+
+
 serialnum = str(pm_settings.sn)
 waveplate = RotationScript.Waveplate(0)
 pm = PowerMeterScript.open_powermeter(serialnumber=serialnum)
 
+# stores the averaged measured power for a certain angle of the waveplate
 power_list = []
+# stores the list of the respective angles of the waveplate
 deg_list = []
 
-for i in np.arange(0, 180, 5):
-    print(str(i) + " degree")
-    deg_list.append(i)
-    waveplate.rotate(i)
+# perform a sample measurement to optimize fit parameters
+for alpha in np.arange(0, 180, 5):
+    print(str(alpha) + " degree")
+    deg_list.append(alpha)
+    waveplate.rotate(alpha)
+    # collect samples for averaging the value for a given angle position alpha
     power_samples = []
     count = 0
     for j in range(100):
         power_sample = PowerMeterScript.measure(pm)
         power_samples.append(power_sample)
         count = count + 1
+    # calculate average power for a given angle position alpha
     average_pow = fsum(power_samples) / count
     power_list.append(average_pow)
     print(str(average_pow) + " appended")
 
 print(power_list, deg_list)
 
+# optimize fit parameters based on the collected sample
 params, params_covariance = optimize.curve_fit(
-    f=fit.sin, xdata=deg_list, ydata=power_list, p0=[3.5e-05, 1, 2, 3.5e-05]
+    f=fit.sin,
+    xdata=deg_list,
+    ydata=power_list,
+    # p0 is optional for curve_fit()
+    # However, appropriate choice of initial values for sine fit is critical
+    p0=[3.5e-05, (2 * np.pi) / 90, 2, 3.5e-05],
 )
+
 print(params, params_covariance)
 
 a, b, c, d = params
@@ -42,13 +64,29 @@ a, b, c, d = params
 plt.scatter(deg_list, power_list, label="DATA")
 
 yvalue_list = []
-for k in range(len(deg_list)):
-    yvalue = fit.sin(x=deg_list[k], a=params[0], b=params[1], c=params[2], d=params[3])
-    print(yvalue)
+xvalue_list = []
+for k in np.arange(0, 360, 0.02):
+    yvalue = fit.sin(x=k, a=params[0], b=params[1], c=params[2], d=params[3])
+    xvalue_list.append(k)
     yvalue_list.append(yvalue)
 
-print(yvalue_list)
+dict = dict(zip(xvalue_list, yvalue_list))
 
-plt.plot(deg_list, yvalue_list, label="FIT", color="red")
+print(dict)
+print("MIN")
+min = min(dict.values())
+print(min)
+
+for x, y in dict.items():
+    if y == min:
+        print(x)
+
+listOfKeys = getKeysByValue(dict, min)
+print("Keys with value equal to MIN")
+# Iterate over the list of keys
+for key in listOfKeys:
+    print(key)
+
+plt.plot(xvalue_list, yvalue_list, label="FIT", color="red")
 plt.legend(loc="best")
 plt.show()
