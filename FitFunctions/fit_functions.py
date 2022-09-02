c6e189015da9e4fcb8d893d59c6ccfdc91b56673
Felix Zilk <zilk.felix@gmail.com>
2022-09-02 16:55:48 +0200

    script tested, fit yet incorrect

---
+++ FitFunctions/fit_functions.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def sin(x, a, b, c, d):
+    return a * np.sin((b * x) + c) + d
--- calibration.py
+++ calibration.py
@@ -1,6 +1,8 @@
 from ControlScripts import RotationScript, PowerMeterScript
 from Settings import pm_settings
-from scipy.optimize import curve_fit
+from FitFunctions import fit_functions as fit
+
+from scipy import optimize
 from math import fsum
 
 import time
@@ -14,7 +16,7 @@ pm = PowerMeterScript.open_powermeter(serialnumber=serialnum)
 power_list = []
 deg_list = []
 
-for i in np.arange(0, 90, 1):
+for i in np.arange(0, 180, 5):
     print(str(i) + " degree")
     deg_list.append(i)
     waveplate.rotate(i)
@@ -30,5 +32,23 @@ for i in np.arange(0, 90, 1):
 
 print(power_list, deg_list)
 
-plt.scatter(deg_list, power_list)
+params, params_covariance = optimize.curve_fit(
+    f=fit.sin, xdata=deg_list, ydata=power_list
+)
+print(params, params_covariance)
+
+a, b, c, d = params
+
+plt.scatter(deg_list, power_list, label="DATA")
+
+yvalue_list = []
+for k in range(len(deg_list)):
+    yvalue = fit.sin(x=deg_list[k], a=params[0], b=params[1], c=params[2], d=params[3])
+    print(yvalue)
+    yvalue_list.append(yvalue)
+
+print(yvalue_list)
+
+plt.plot(deg_list, yvalue_list, label="FIT", color="red")
+plt.legend(loc="best")
 plt.show()
